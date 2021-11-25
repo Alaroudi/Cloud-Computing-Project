76a7587381a0e6eea338cd1c694e58d3accb5168
Connor <cdm686@my.utsa.edu>
2021-11-24 21:29:44 -0600

    Frontend done, need firebase connections

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { Route, useRouteMatch } from "react-router";
 import List from "./components/List";
 
 const App = () => {
+  const [text, setText] = useState("New List");
   const [lists, setLists] = useState([]);
   useEffect(() => {
     const unsub = onSnapshot(collection(db, "List_Maker"), snapshot => {
@@ -18,6 +19,11 @@ const App = () => {
   const match = useRouteMatch();
   console.log(match.params);
 
+  function addList() {
+    //Add a new list to firebase
+    console.log("Add the list '" + text + "' to firebase!");
+  }
+
   return (
     <div className="main-container">
       <div className="menu-container">
@@ -25,6 +31,8 @@ const App = () => {
           <img src="./images/logo.png" width="100%" alt="" />
         </div>
         <MenuLists lists={lists} />
+        <input className="menuBtn" onClick={addList} type="button" value="Add List"/>
+        <input id="text" className="menuText" onChange={event => setText(event.target.value)} type="textField" value={text}/>
       </div>
 
       <Route
--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,18 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 
 const List = ({ lists, match }) => {
+
+  const [text, setText] = useState("New Item");
+
   const list = lists.find(list => list.id === match.params.id);
   if (!list) return <h1>List is not found</h1>;
+
+  function addItem() {
+    //Add a firebase list element
+    console.log("Add '" + text + "' as an item to this list!");
+  }
+
+  function removeItem() {
+    //Remove a firebase list element
+    console.log("Remove the last item in the list: " + list.list_items[list.list_items.length-1].name);
+  }
+
+  function deleteList() {
+    //Remove this list and reroute to home
+    console.log("This will delete " + list.list_name);
+  }
+
+  function handleCheck(name) {
+    //Register checkbox status on firebase
+    console.log("Change the value of the checkbox for '" + name + "'");
+  }
+
   return (
     <div className="list-viewer-container">
       <h1>{list.list_name}</h1>
         {list.list_items.map((item, index) => (
           <div id="item-name" key={index}>
-            <input id="check" type="checkbox"/>
+            <input id="check" onChange={() => handleCheck(item.name)} type="checkbox"/>
             <label className="list-item" for="check">{item.name}</label>
           </div>
           //<li key={index}>{item.name}</li> - original solution
         ))}
+        <br/>
+        <input className="listBtn" onClick={addItem} type="button" value="Add Item"/>
+        <input className="listBtn" onClick={removeItem} type="button" value="Remove Item"/>
+        <input className="listBtn" onClick={deleteList} type="button" value="Delete List"/> <br/>
+        <input className="listText" onChange={event => setText(event.target.value)} type="textField" value={text}/>
     </div>
   );
 };
